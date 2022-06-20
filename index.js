a80b7ed1bcd77777b0875c3d7f490be80fc0ec37
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-06-19 21:31:57 -0300

    post_tweet_OK

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.post("/sign-up", (req, res) => {
     res.send("Ok");
 });
 
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send("Ok");
+});
+
 
 
 app.listen(5000, ()=> {
