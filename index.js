1b94a1a3875061ce711c3ed320f6c0b3fb80e0da
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-06-20 03:46:42 -0300

    finalizado

--- index.js
+++ index.js
@@ -11,19 +11,35 @@ const users = [];
 const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.send("Ok");
+  const user = req.body;
+  users.push(user);
+  res.send("Ok");
 });
 
 app.post("/tweets", (req, res) => {
-    const tweet = req.body;
-    tweets.push(tweet);
-    res.send("Ok");
+  const tweet = req.body;
+  tweets.unshift(tweet);
+  res.send("Ok");
 });
 
-
+app.get("/tweets", (req, res) => {
+  const lastTweets = [];
+    if(tweets.length > 0){
+      for(let i = 0; i < tweets.length && i < 10; i++){
+        const avatar = users.find(element => element.username === tweets[i].username);
+        const username = tweets[i].username;
+        const tweet = tweets[i].tweet;
+        const obj = {
+          username,
+          avatar: avatar.avatar,
+          tweet
+        };
+        lastTweets.push(obj);
+      };
+  }
+  res.send(lastTweets);
+});
 
 app.listen(5000, ()=> {
-
+  console.log(chalck.green("online"));
 });
